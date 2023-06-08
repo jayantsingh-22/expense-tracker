f87e111a31b11ad3129fecea24dacc75d1155f56
jayantsingh-22 <jayantsingh640@gmail.com>
2023-06-08 19:48:30 +0530

    added:expense item component and its styling

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate.js";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title); 
+
+  const clickHandler = () => {
+    setTitle("updated!");
+    console.log(title);
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">INR {props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
